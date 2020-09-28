e53e9b4ebd47564795f9d32817ebff2ceb2a5fca
chrlag <chr.laganas@gmail.com>
2020-09-28 20:26:49 +0300

    CHRLAGIW-19 Completed

--- Milestone 1/InABucket/src/main/java/com/sg/inabucket/InABucket.java
+++ Milestone 1/InABucket/src/main/java/com/sg/inabucket/InABucket.java
@@ -10,5 +10,22 @@ package com.sg.inabucket;
  * @author Chris
  */
 public class InABucket {
+    public static void main(String[] args){
+        
+        // You can declare all kinds of variables
+        String walrus;
+        double piesEaten;
+        float weightOfTeacupPig;
+        int grainsOfSand;
+        
+        // But declaring them just sets up the space in the data
+        // to use the variable, you have to put data IN it first!
+        walrus = "Sir Leory Jenkins III";
+        piesEaten = 42.1;
+        
+        System.out.println("Meet my pet Walrus, " + walrus);
+        System.out.print("He was a bit hungry today, and ate this many pies : ");
+        System.out.println("piesEaten");
+    }
     
 }
