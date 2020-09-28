7bfa1b4cadec3e899e3ed38178e8fc9352df6f42
chrlag <chr.laganas@gmail.com>
2020-09-28 21:40:42 +0300

    CHRLAGIW-34 Completed

---
+++ Milestone 1/BiggerBetterAdder/src/main/java/com/sg/biggerbetteradder/BiggerBetterAdder.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.biggerbetteradder;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Chris
+ */
+public class BiggerBetterAdder {
+    public static void main(String[] args){
+        
+        int number1, number2, number3;
+        int sum;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please type the first number");
+        number1 = Integer.parseInt(sc.nextLine());
+        
+        System.out.println("Please type the second number");
+        number2 = Integer.parseInt(sc.nextLine());
+        
+        System.out.println("Please type the third number");
+        number3 = Integer.parseInt(sc.nextLine());
+        
+        System.out.println("Number 1 is: " + number1);
+        System.out.println("Number 2 is: " + number2);
+        System.out.println("Number 3 is: " + number3);
+        
+        sum = number1 + number2 + number3;
+        
+        System.out.println("The sum is: " + sum);
+        System.out.println("One more time " + sum);
+        
+  
+    }
+    
+}
