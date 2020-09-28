fa61f1e4345295113d7a9c67b8fa7ef2596cbfab
chrlag <chr.laganas@gmail.com>
2020-09-28 21:07:23 +0300

    CHRLAGIW-23 Completed

---
+++ Milestone 1/AllAboutMe/src/main/java/com/sg/allaboutme/AllAboutMe.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.allaboutme;
+
+/**
+ *
+ * @author Chris
+ */
+public class AllAboutMe {
+    public static void main(String[] args){
+        
+        String fullName = "Chris Laganas";
+        String food = "soup";
+        int numOfPets = 1;
+        boolean eatenGnocchi = true;
+        int ageOfWhistle = 11;
+        
+        System.out.println("My full name is " + fullName);
+        System.out.println("My favorite food is " + food);
+        System.out.println("I have" + numOfPets + " number of pets");
+        System.out.println("It is " + eatenGnocchi + " that I have eaten Gnocchi.");
+        System.out.println("I've learned to whistle at the age of " + ageOfWhistle);
+                
+    }
+    
+}
