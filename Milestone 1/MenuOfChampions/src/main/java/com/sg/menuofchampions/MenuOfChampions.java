6b7d406f57518c65721d86b058bda58817af6523
chrlag <chr.laganas@gmail.com>
2020-09-28 21:19:29 +0300

    CHRLAGIW-24 Completed

---
+++ Milestone 1/MenuOfChampions/src/main/java/com/sg/menuofchampions/MenuOfChampions.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.menuofchampions;
+
+/**
+ *
+ * @author Chris
+ */
+public class MenuOfChampions {
+    public static void main(String[] args){
+        
+        String name = "Duket";
+        String food1 = "Pizza";
+        float valueFood1 = 10.00f;
+        String food2 = "Gyros";
+        float valueFood2 = 2.80f;
+        String food3 = "Burger";
+        float valueFood3 = 5.00f;
+        
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println();
+        System.out.println("              Welcome to " + name + "!");
+        System.out.println("              Today's menu is...");
+        System.out.println();
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println();
+        System.out.println("          " + food1 + "      $" + valueFood1);
+        System.out.println("          " + food2 + "      $" + valueFood2);
+        System.out.println("          " + food3 + "     $" + valueFood3);
+                
+        
+        
+    }
+    
+}
