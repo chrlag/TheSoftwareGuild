93c657939df1cb63de04be5a3909a03b7e0ede5f
chrlag <chr.laganas@gmail.com>
2020-10-01 21:45:39 +0300

    CHRLAGIW-83 Completed

---
+++ Milestone 1/HiddenNuts/src/main/java/com/sg/hiddennuts/HiddenNuts.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.hiddennuts;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Chris
+ */
+public class HiddenNuts{
+    public static void main(String[] args){
+        
+        String[] hidingSpots = new String[100];
+        Random squirrel = new Random();
+        hidingSpots[squirrel.nextInt(hidingSpots.length)] = "Nut";
+        System.out.println("The nut has been hidden ... ");
+        
+        for (int i = 0; i < hidingSpots.length; i++){
+            if (hidingSpots[i] != null){
+                System.out.println(i);
+            }
+        }
+        
+        
+    }
+        
+    
+}
