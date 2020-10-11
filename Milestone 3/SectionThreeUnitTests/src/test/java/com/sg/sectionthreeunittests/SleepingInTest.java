3c59e16f4c3a11e7258d105911e865d6a423faf8
chrlag <chr.laganas@gmail.com>
2020-10-11 22:58:20 +0300

    CHRLAGIW-119 Completed

---
+++ Milestone 3/SectionThreeUnitTests/src/test/java/com/sg/sectionthreeunittests/SleepingInTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.sectionthreeunittests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Chris
+ */
+public class SleepingInTest {
+    
+    private SleepingIn sleep = new SleepingIn();
+               
+    public SleepingInTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testWeekdayVacationTrue() {
+        assertTrue(sleep.canSleepIn(true, true));   
+    }
+
+    @Test
+    public void testNotWeekdayVacationTrue() {
+        assertTrue(sleep.canSleepIn(false, true));   
+    }  
+    
+    @Test
+    public void testNotWeekdayNotVacationTrue() {
+        assertTrue(sleep.canSleepIn(false, false));   
+    }
+    
+    @Test
+    public void testWeekdayNotVacationFalse() {
+        assertFalse(sleep.canSleepIn(true, false));   
+    }       
+}
