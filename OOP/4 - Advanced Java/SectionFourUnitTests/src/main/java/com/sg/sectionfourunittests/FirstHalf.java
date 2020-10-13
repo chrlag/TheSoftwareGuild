0bfb7a1de75b06413859e99020b4c6bdd7ea7f13
chrlag <chr.laganas@gmail.com>
2020-10-13 17:19:59 +0300

    CHRLAGIW-173 Completed

---
+++ OOP/4 - Advanced Java/SectionFourUnitTests/src/main/java/com/sg/sectionfourunittests/FirstHalf.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.sectionfourunittests;
+
+/**
+ *
+ * @author Chris
+ */
+public class FirstHalf {
+    // Given a String of even length, return the first half. 
+    // So the String "WooHoo" yields "Woo". 
+    //
+    // firstHalf("WooHoo") -> "Woo"
+    // firstHalf("HelloThere") -> "Hello"
+    // firstHalf("abcdef") -> "abc"
+    public String firstHalf(String str) {
+        
+        String returnString = "";
+        
+        for (int i = 0; i < str.length() / 2; i++){
+            returnString += str.charAt(i);
+        }
+        
+        return returnString;
+    }
+   
+}
