789c461e70de04e6f9d1847941d7f413444ae844
chrlag <chr.laganas@gmail.com>
2020-10-13 14:20:16 +0300

    changes in folders

---
+++ 1 - Basic Java/BestAdderEver/src/main/java/com/sg/bestadderever/BestAdderEver.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.bestadderever;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Chris
+ */
+public class BestAdderEver {
+    public static void main(String[] args){
+        
+        int number1 = 5, number2 = 3, number3 = 5;
+        int sum;
+    
+        
+        System.out.println("Number 1 is: " + number1);
+        System.out.println("Number 2 is: " + number2);
+        System.out.println("Number 3 is: " + number3);
+        
+        sum = number1 + number2 + number3;
+        
+        System.out.println("The sum is: " + sum);
+        System.out.println("One more time " + sum);
+        
+
+    }
+    
+}
