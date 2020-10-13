60807e8b6232ebb85ca2f9861b7e72969711608b
chrlag <chr.laganas@gmail.com>
2020-10-14 01:46:33 +0300

    removed comments in App.java

--- OOP/3 - Intermediate Java/.Project/ClassRoster/src/main/java/com/sg/classroster/App.java
+++ OOP/3 - Intermediate Java/.Project/ClassRoster/src/main/java/com/sg/classroster/App.java
@@ -16,15 +16,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     
     public static void main(String[] args) {
-//        UserIO myIo = new UserIOConsoleImpl();
-//       ClassRosterView myView = new ClassRosterView(myIo);
-//        ClassRosterDao myDao = new ClassRosterDaoFileImpl();
-//        ClassRosterAuditDao myAuditDao = new ClassRosterAuditDaoFileImpl();
-//       ClassRosterServiceLayer myService = 
-//               new ClassRosterServiceLayerImpl(myDao, myAuditDao);
-//        ClassRosterController controller = 
-//                new ClassRosterController(myService, myView);
-//        controller.run();
 
         ApplicationContext ctx = 
                 new ClassPathXmlApplicationContext("applicationContext.xml");
